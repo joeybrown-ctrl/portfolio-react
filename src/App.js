d2b02734ba373edf5d92a63ca39ae97432279002
Joey Brown <gjoey.brown@gmail.com>
2021-01-13 17:52:00 -0800

    disabled Hero component within App.js to stop an infinite loop.

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+// import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Hero>
+  
     <Router>
+    <Navbar />
+    {/* <Hero> */}
       <Switch>
         <Route exact path="/" component={Main} />
         <Route exact path="/About" component={About} />
         <Route exact path="/Portfolio" component={Portfolio} />
         <Route path="/Contact" component={Contact} />
       </Switch>
-    </Router>
-    </Hero>
+    {/* </Hero> */}
     <Footer />
+    </Router>
+    
     </>
   );
 }
