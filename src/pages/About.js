b95cb36be273d7eae3ba28d0128112cb068d6ca7
Joey Brown <gjoey.brown@gmail.com>
2021-02-13 19:10:38 -0800

    edited copy on about page, fixed project in portfolio

--- src/pages/About.js
+++ src/pages/About.js
@@ -30,6 +30,7 @@ function About() {
                 <div className="row">
                     <div className="col text-center">
                         <h2 className="text-white" style={styles.h2}>About Me</h2>
+                        <hr />
                     </div>
                 </div>
                 <div className="row">
@@ -40,13 +41,15 @@ function About() {
 
                     <div className="col-sm-12 col-md-6 text-white">
                         <p>
-                            Kia Ora! I am a Full-Stack developer certified through the University of Arizona/Trilogy's
-                            Coding Bootcamp. I am passionate about the
-                            continual learning process that programming requires. My technical skills include HTML 5, CSS 3,
-                            Bootstrap 4, Bulma, Git, GitHub, JavaScript,
-                            jQuery, JSON/BSON, AJAX, REST APIs, Node.js, Express and Express-Handlebars, MySQL, Sequelize,
-                            MVC, MongoDB/Mongoose and React.
+                            Hey there! I am a certified full-stack web developer. I am a MERN developer, so I work mainly with JavaScript,
+                            with a focus on React and Node. I am passionate about the balance between design and functionality,
+                            and dedicated to the continual learning process that lays at the heart of development.
                         </p>
+                        <hr />
+                        <p>
+                            My skills include JavaScript, React, Node, MySQL/NoSQL, ORMs, PWAs, Webpack, CSS, Bootstrap, and Gatsby.
+                        </p>
+                        <hr />
                         <p>
                             Before my entrance into the tech field, I worked as a bartender and bar manager specializing in
                             craft cocktails in Seattle, San Francisco and
@@ -56,10 +59,16 @@ function About() {
                             understand both people and systems. In my spare time, I can
                             be found cooking delicious meals, working with plants, and writing songs on my guitar.
                         </p>
+                        <hr />
                         <p>
                             I hold dual citizenship in New Zealand and the United States, and have the right to work in both countries.
                             I am available for work at this time.
                         </p>
+
+                        <div><a href="https://docs.google.com/document/d/1PhYnlpJAlFEQCuOxQ33QhitccT5LWES6N_FZJKTaWWA/edit?usp=sharing" className="btn-floating btn-lg" role="button" target="_blank" rel="noreferrer">
+                            <h4 className="text-white" rel="noreferrer">View Resume</h4>
+                        </a>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -24,148 +24,150 @@ function Portfolio() {
     }
 
 
-    return(
+    return (
         <>
-        <Navbar />
-        <div className="container" style={styles.container}>
-            <div className="row justify-content-center">
-                <h2 className="text-white" style={styles.h2}>Portfolio
-                </h2>
-                <br/>
-            </div>
+            <Navbar />
 
-            <div className="row justify-content-center">
-                <div className="col-lg-4">
-                    <div className="card mx-auto mb-3" style={styles.card}>
-                        <img className="card-img-top" src={process.env.PUBLIC_URL + "/assets/beercrawlr-sm.png"} alt="Beercrawlr project"/>
-                        <hr/>
-                        <h5 className="card-title text-center">BeerCrawlr</h5>
-                        <div className="card-body text-center">
-                            <p className="card-text">A brewery and restaurant search app that I created with a team of four.
-                                Utilizes JavaScript, jQuery, AJAX, and APIs. Front-end built using HTML5 and Bulma. </p>
-                            <a href="https://joeybrown-ctrl.github.io/Beercrawlr/" className="btn btn-success"
-                                target="_blank" rel="noreferrer">View Project</a>
-                            <hr/>
-                            <a href="https://github.com/joeybrown-ctrl/Beercrawlr" className="btn btn-success"
-                                target="_blank" rel="noreferrer">View Repository</a>
-                        </div>
-
-                    </div>
+            <div className="container" style={styles.container}>
+                <div className="row justify-content-center">
+                    <h2 className="text-white" style={styles.h2}>Portfolio
+                    </h2>
+                    <br />
                 </div>
 
-                <div className="col-lg-4">
+                <div className="row justify-content-center">
+                    <div className="col-lg-4">
+                        <div className="card mx-auto mb-3" style={styles.card}>
+                            <img className="card-img-top" src={process.env.PUBLIC_URL + "/assets/beercrawlr-sm.png"} alt="Beercrawlr project" />
+                            <hr />
+                            <h5 className="card-title text-center">BeerCrawlr</h5>
+                            <div className="card-body text-center">
+                                <p className="card-text">A brewery and restaurant search app that I created with a team of four.
+                                    Utilizes JavaScript, jQuery, AJAX, and APIs. Front-end built using HTML5 and Bulma. </p>
+                                <a href="https://joeybrown-ctrl.github.io/Beercrawlr/" className="btn btn-success"
+                                    target="_blank" rel="noreferrer">View Project</a>
+                                <hr />
+                                <a href="https://github.com/joeybrown-ctrl/Beercrawlr" className="btn btn-success"
+                                    target="_blank" rel="noreferrer">View Repository</a>
+                            </div>
 
-                    <div className="card mx-auto mb-3" style={styles.card} >
-                        <img className="card-img-top" src={process.env.PUBLIC_URL + "/assets/odyssey.png"} alt="Odyssey's landing page"/>
-                        <hr/>
-                        <h5 className="card-title text-center">Odyssey</h5>
-                        <div className="card-body text-center">
-                            <p className="card-text">A scrapbooking travel app that allows the user to create
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4">
+
+                        <div className="card mx-auto mb-3" style={styles.card} >
+                            <img className="card-img-top" src={process.env.PUBLIC_URL + "/assets/odyssey.png"} alt="Odyssey's landing page" />
+                            <hr />
+                            <h5 className="card-title text-center">Odyssey</h5>
+                            <div className="card-body text-center">
+                                <p className="card-text">A scrapbooking travel app that allows the user to create
                                 country-specific travelogues filled with pictures and notes. The app utilizes the MVC
                                 design pattern, and is built with Node.js, Handlebars.js, Sequelize, Cloudinary,
-                                SendGrid and RESTCountries. Deployed to Heroku with a jawsDB database. </p>
-                            <a href="https://team-zeus.herokuapp.com/" className="btn btn-success" target="_blank" rel="noreferrer">View
-                                Project</a>
-                            <hr/>
-                            <a href="https://github.com/joeybrown-ctrl/Odyssey" className="btn btn-success"
-                                target="_blank" rel="noreferrer">View Repository</a>
-                        </div>
+                                    SendGrid and RESTCountries. Deployed to Heroku with a jawsDB database. </p>
+                                <a href="https://team-zeus.herokuapp.com/" className="btn btn-success" target="_blank" rel="noreferrer">View
+                                    Project</a>
+                                <hr />
+                                <a href="https://github.com/joeybrown-ctrl/Odyssey" className="btn btn-success"
+                                    target="_blank" rel="noreferrer">View Repository</a>
+                            </div>
 
+                        </div>
                     </div>
+
+                    <div className="col-lg-4">
+                        <div className="card mx-auto mb-3" style={styles.card}>
+                            <img className="card-img-top"
+                                src="https://raw.githubusercontent.com/joeybrown-ctrl/Employee-Directory/main/public/assets/EmpDir.png" alt="Employee Directory" />
+                            <hr />
+                            <h5 className="card-title text-center">Employee Directory</h5>
+                            <div className="card-body text-center">
+                                <p className="card-text">An employee directory app that allows the user to filter through employees by name. Built with React, React hooks, and Bootstrap.</p>
+                                <a href="https://joeybrown-ctrl.github.io/Employee-Directory/" className="btn btn-success"
+                                    target="_blank" rel="noreferrer">View Project</a>
+                                <hr />
+                                <a href="https://github.com/joeybrown-ctrl/Employee-Directory" className="btn btn-success"
+                                    target="_blank" rel="noreferrer">View Repository</a>
+                            </div>
+
+                        </div>
+                    </div>
+
                 </div>
 
-                <div className="col-lg-4">
-                    <div className="card mx-auto mb-3" style={styles.card}>
-                        <img className="card-img-top"
-                            src="https://raw.githubusercontent.com/joeybrown-ctrl/Employee-Directory/main/public/assets/EmpDir.png" alt="Team profile generator"/>
-                        <hr/>
-                        <h5 className="card-title text-center">Team Profile Generator</h5>
-                        <div className="card-body text-center">
-                            <p className="card-text">An employee directory app that allows the user to filter through employees by name. Built with React, React hooks, and Bootstrap.</p>
-                            <a href="https://joeybrown-ctrl.github.io/Employee-Directory/" className="btn btn-success"
-                                target="_blank" rel="noreferrer">View Project</a>
-                            <a href="https://github.com/joeybrown-ctrl/Employee-Directory" className="btn btn-success"
-                                target="_blank" rel="noreferrer">View Repository</a>
-                        </div>
+                <hr />
+                <br />
+                <br />
+                <hr />
 
-                    </div>
-                </div>
+                <div className="row justify-content-center">
+                    <div className="col-lg-4">
 
-                <div className="col-lg-4">
-
-                    <div className="card mx-auto mb-3" style={styles.card}>
-                        <img className="card-img-top" src={process.env.PUBLIC_URL + "/assets/burger-logger.png"} alt="burger-logger app"/>
-                        <hr/>
-                        <h5 className="card-title text-center">Burger Logger</h5>
-                        <div className="card-body text-center">
-                            <p className="card-text">A burger logger app built with MySQL, Node, Express, Handlebars and a
+                        <div className="card mx-auto mb-3" style={styles.card}>
+                            <img className="card-img-top" src={process.env.PUBLIC_URL + "/assets/burger-logger.png"} alt="burger-logger app" />
+                            <hr />
+                            <h5 className="card-title text-center">Burger Logger</h5>
+                            <div className="card-body text-center">
+                                <p className="card-text">A burger logger app built with MySQL, Node, Express, Handlebars and a
                                 homemade ORM. Created following the MVC design pattern, and deployed to Heroku. Node and
                                 MySQL are used to query and route data through the app, and Handlebars generates the
-                                HTML and Bootstrap front-end.</p>
-                            <a href="https://burger-logger-joeybrown-ctrl.herokuapp.com/" className="btn btn-success"
-                                target="_blank" rel="noreferrer">View Project</a>
-                            <hr/>
-                            <a href="https://github.com/joeybrown-ctrl/burger-logger" className="btn btn-success"
-                                target="_blank" rel="noreferrer">View Repository</a>
+                                    HTML and Bootstrap front-end.</p>
+                                <a href="https://burger-logger-joeybrown-ctrl.herokuapp.com/" className="btn btn-success"
+                                    target="_blank" rel="noreferrer">View Project</a>
+                                <hr />
+                                <a href="https://github.com/joeybrown-ctrl/burger-logger" className="btn btn-success"
+                                    target="_blank" rel="noreferrer">View Repository</a>
+                            </div>
+
                         </div>
-
                     </div>
-                </div>
 
-            </div>
-            <hr/>
-            <br/>
-            <br/>
 
-            <div className="row justify-content-center">
-                <div className="col-lg-4">
+                    <div className="col-lg-4">
 
-                    <div className="card mx-auto mb-3" style={styles.card}>
-                        <img className="card-img-top"
-                            src="https://raw.githubusercontent.com/joeybrown-ctrl/joeybrown-ctrl.github.io/master/images/note-taker-pic.png"
-                            alt="Note-Taker app"/>
-                        <hr/>
-                        <h5 className="card-title text-center">Note Taker</h5>
-                        <div className="card-body text-center">
-                            <p className="card-text">A note-taking app for users to keep persistent notes. Allows users to
+                        <div className="card mx-auto mb-3" style={styles.card}>
+                            <img className="card-img-top"
+                                src="https://raw.githubusercontent.com/joeybrown-ctrl/joeybrown-ctrl.github.io/master/images/note-taker-pic.png"
+                                alt="Note-Taker app" />
+                            <hr />
+                            <h5 className="card-title text-center">Note Taker</h5>
+                            <div className="card-body text-center">
+                                <p className="card-text">A note-taking app for users to keep persistent notes. Allows users to
                                 create, save and delete notes, as well as viewing previously saved notes. Utilizes an
                                 Express back-end and uses GET, POST and DELETE methods with HTML and API routes to work
                                 with
                                 data. Built using Express.js, Node.js, JavaScript, CSS and HTML. Deployed via Heroku.
-                            </p>
-                            <a href="https://note-taker-joeybrown-ctrl.herokuapp.com/" className="btn btn-success"
-                                target="_blank" rel="noreferrer">View Project</a>
-                            <hr/>
-                            <a href="https://github.com/joeybrown-ctrl/note-taker" className="btn btn-success"
-                                target="_blank" rel="noreferrer">View Repository</a>
+                                </p>
+                                <a href="https://note-taker-joeybrown-ctrl.herokuapp.com/" className="btn btn-success"
+                                    target="_blank" rel="noreferrer">View Project</a>
+                                <hr />
+                                <a href="https://github.com/joeybrown-ctrl/note-taker" className="btn btn-success"
+                                    target="_blank" rel="noreferrer">View Repository</a>
+                            </div>
                         </div>
                     </div>
-                </div>
 
+                    <div className="col-lg-4">
 
-                <div className="col-lg-4">
-
-                    <div className="card mx-auto mb-3" style={styles.card}>
-                        <img className="card-img-top" src={process.env.PUBLIC_URL + "/assets/emp-tracker.png"} alt="Employee Tracker app"/>
-                        <hr/>
-                        <h5 className="card-title text-center">Employee Tracker</h5>
-                        <div className="card-body text-center">
-                            <p className="card-text">A command-line application that allows users with businesses to track
+                        <div className="card mx-auto mb-3" style={styles.card}>
+                            <img className="card-img-top" src={process.env.PUBLIC_URL + "/assets/emp-tracker.png"} alt="Employee Tracker app" />
+                            <hr />
+                            <h5 className="card-title text-center">Employee Tracker</h5>
+                            <div className="card-body text-center">
+                                <p className="card-text">A command-line application that allows users with businesses to track
                                 employees by viewing, adding, updating and deleting employees, their roles, and
                                 departments. Built using Node.js with the Inquirer and MySQL NPM packages, Visual Studio
-                                Code, and MySQL Workbench.</p>
-                            <a href="https://github.com/joeybrown-ctrl/employee-tracker" className="btn btn-success"
-                                target="_blank" rel="noreferrer">View Project</a>
-                        </div>
+                                    Code, and MySQL Workbench.</p>
+                                <a href="https://github.com/joeybrown-ctrl/employee-tracker" className="btn btn-success"
+                                    target="_blank" rel="noreferrer">View Project</a>
+                            </div>
 
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <br/>
-        <hr/>
-        <br/>
+
         </>
     )
 }
